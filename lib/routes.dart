041fc8d61624c0d113466b50ec018e3140d98f74
Alan Pinho <alan@yardim.com.br>
2025-10-23 15:22:34 -0300

    adicionando tela splash temporaria

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,7 @@
+import 'package:colecionismo/views/splash/splash_view.dart';
 import 'package:flutter/material.dart';
 
 class Routes {
   static Map<String, Widget Function(BuildContext)> get routingTable =>
-      <String, WidgetBuilder>{};
+      <String, WidgetBuilder>{SplashView.routeName: (_) => SplashView()};
 }
---
+++ lib/views/splash/splash_view.dart
@@ -0,0 +1,53 @@
+import 'package:colecionismo/views/shared/cl_loader.dart';
+import 'package:colecionismo/views/shared/cl_spacer.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  static String routeName = '/';
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: _Body()));
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Spacer(),
+          Text(
+            'Colecionismo',
+            style: Theme.of(context).textTheme.displayMedium?.copyWith(
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).colorScheme.tertiary,
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Text(
+              'Te ajudando a organizar sua coleção...',
+              textAlign: TextAlign.end,
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).colorScheme.tertiary,
+              ),
+            ),
+          ),
+          Spacer(),
+          ClLoader(),
+          ClSpacer.vertical(2),
+        ],
+      ),
+    );
+  }
+}
